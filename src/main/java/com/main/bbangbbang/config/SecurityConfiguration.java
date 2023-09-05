ec05e63f1ac5453ce3616a7252811aa3e232b930
kimhaming <kimhamin83@gmail.com>
2023-09-05 17:00:17 +0900

    ✨CORS 설정에 프론트엔드 서버 허용

--- src/main/java/com/main/bbangbbang/config/SecurityConfiguration.java
+++ src/main/java/com/main/bbangbbang/config/SecurityConfiguration.java
@@ -7,9 +7,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.transaction.TransactionDefinition.withDefaults;
 
+//
 @Configuration
 public class SecurityConfiguration {
     @Bean
@@ -50,5 +56,24 @@ public class SecurityConfiguration {
 
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+            configuration.setAllowedOrigins(Arrays.asList(
+                    "http://localhost:3000",    // 로컬 개발 환경의 프론트엔드 서버 주소 허용
+                    "http://bbangorder.s3-website.ap-northeast-2.amazonaws.com" // S3에 호스팅된 프론트엔드 웹사이트 서버 주소 허용
+            ));
+
+            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+            configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
