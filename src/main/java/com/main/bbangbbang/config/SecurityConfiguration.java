c7cbd1ab0e425c1993e7259595ca7b13b1fac180
kimhaming <kimhamin83@gmail.com>
2023-09-06 04:24:22 +0900

    ✨OAuth2 로그인에 성공/실패 시 수행할 처리

--- src/main/java/com/main/bbangbbang/config/SecurityConfiguration.java
+++ src/main/java/com/main/bbangbbang/config/SecurityConfiguration.java
@@ -1,23 +1,36 @@
 package com.main.bbangbbang.config;
 
+import com.main.bbangbbang.utils.CustomAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+//  OAuth2 사용자가 로그인 성공 시에 대한 성공 및 실패에 대한 특정 핸들러 작업으로 처리로 변경(버전업)해주며 불필요
+//  import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-
-import static org.springframework.transaction.TransactionDefinition.withDefaults;
+//  사용자 정보 기본값 -> UserInfo 변경되면서 불필요
+//  import static org.springframework.transaction.TransactionDefinition.withDefaults;
 
 //
 @Configuration
 public class SecurityConfiguration {
+
+    //  JwtTokenizer 객체를 빈으로 등록
+    @Bean
+    public JwtTokenizer tokenizer(){
+        return new JwtTokenizer();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -52,7 +65,15 @@ public class SecurityConfiguration {
                                 .antMatchers(HttpMethod.DELETE, "/*/orders").hasRole("USER")
                                 .anyRequest().permitAll()
                 )
-                .oauth2Login(Customizer.withDefaults());  // (1)
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(new OAuth2LoginSuccessHandler(
+                                tokenizer(),    // tokenizer() 메서드 호출하여 인스턴스 사용
+                                new CustomAuthorityUtils()
+                        ))
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("OAuth2LoginAuthenticationFailureHandler");
+                            exception.printStackTrace();
+                        }));  // (1)
 
 
         return http.build();
